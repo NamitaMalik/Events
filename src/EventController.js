4e1f9c2db97ea053e90c2ee18090e0688d597e66
Amit <amit2.kumar@paytm.com>
2015-03-22 20:48:35 +0530

    Correcting css.

---
+++ src/EventController.js
@@ -0,0 +1,34 @@
+/**
+ * Created by namita on 13/3/15.
+ */
+(function (ng) {
+  var eventApp = ng.module('eventApp', []);
+  eventApp.controller('EventCtrl', ["$scope", "EventService", function ($scope, EventService) {
+    $scope.news = [];
+    function getFeeds(url, index) {
+      EventService.getFeeds(url)
+        .success(function (response) {
+          $scope.news[index] = response;
+
+        })
+        .error(function (status) {
+          console.log(status);
+        });
+    }
+
+    function getFeedSource() {
+      EventService.getFeedSource()
+        .success(function (response) {
+          $scope.feedSources = response;
+          angular.forEach($scope.feedSources, function (value, index) {
+            getFeeds(value.url, index)
+          });
+        })
+        .error(function (status) {
+          console.log(status);
+        });
+    }
+
+    getFeedSource();
+  }]);
+})(angular);
